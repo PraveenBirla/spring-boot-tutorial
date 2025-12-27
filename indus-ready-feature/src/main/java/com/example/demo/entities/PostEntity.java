eb26b04ae9725b94e901181484173ef851a38000
pareveen-birla <birlap409@gmail.com>
2025-12-27 12:12:08 +0530

    learn Auditing

---
+++ indus-ready-feature/src/main/java/com/example/demo/entities/PostEntity.java
@@ -0,0 +1,24 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="posts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostEntity  extends AuditableEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    private String title;
+
+    private String description;
+
+}
